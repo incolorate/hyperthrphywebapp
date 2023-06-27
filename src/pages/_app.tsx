576dc0d79af4978344a03868974ca206b55f97f0
incolorate <88613908+incolorate@users.noreply.github.com>
2023-06-27 19:47:17 +0300

    Home page template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs/dist/types/components.server";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
