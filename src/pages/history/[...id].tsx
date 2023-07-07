31c676c94482b3a90d528306c3d50c6f7d3b5c24
incolorate <88613908+incolorate@users.noreply.github.com>
2023-07-07 13:42:40 +0300

    Render after loading

--- src/pages/history/[...id].tsx
+++ src/pages/history/[...id].tsx
@@ -3,17 +3,21 @@ import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 import { Exercise } from "~/components/Exercise";
 import { useState, useEffect } from "react";
+
 export default function EditWorkout() {
   const router = useRouter();
   const { id } = router.query;
   const numberId = Number(id);
-  const { data: workoutInfo } = api.exercises.findSpecificWorkout.useQuery({
+  const {
+    data: workoutInfo,
+    isLoading,
+    isError,
+  } = api.exercises.findSpecificWorkout.useQuery({
     id: numberId,
   });
+
   const [exercises, setExercises] = useState<string[]>([]);
   const setInfo = workoutInfo?.sets;
-
-  // Get unique exercises and render an exercise element based on that
   useEffect(() => {
     const uniqueExercises: string[] = [];
     setInfo?.map((set) => {
@@ -23,15 +27,22 @@ export default function EditWorkout() {
         // push exercise name to array
         uniqueExercises.push(set.exercise.name);
       }
-
       setExercises(uniqueExercises);
     });
-  }, []);
+  }, [setInfo]);
+
+  if (isLoading) {
+    return <p>Loading...</p>; // Render a loading state while the data is being fetched
+  }
+
+  if (isError) {
+    return <p>Error occurred while loading workout data</p>; // Render an error state if there is an error during the request
+  }
 
   return (
     <Layout>
       {exercises.map((exerciseName, index) => {
-        let exerciseInfo = setInfo?.filter(
+        const exerciseInfo = setInfo?.filter(
           (exercise) => exercise.exercise.name === exerciseName
         );
         console.log(exerciseInfo);
